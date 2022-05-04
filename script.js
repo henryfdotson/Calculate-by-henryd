bc140d77f0aa486a9882d16bf16e013937db6e2f
Henry F. Dotson <henryfdotson@gmail.com>
2022-05-04 15:17:34 -0400

    Getting number values
    Setting up methods to start acquiring values and operator.

--- script.js
+++ script.js
@@ -1,12 +1,45 @@
 const calculator = document.getElementById('calculator');
+var resultDisplay = document.getElementById('result');
 var value1;
 var value2;
+var number = 0;
+var firstValueAquired = false;
+var operatorAquired = false;
 
-function getFirstValue(number) {
-  value1 = number;
-  return value1
+// Methods
+
+function changeButtonColor(color) {
+  var btns = document.getElementsByClassName('calc-button');
+  for(i = 0; i < btns.length; i++) {
+    btns[i].style.backgroundColor = color;
+  }
 }
-function getSecondValue(number) {
-  value1 = number;
-  return value1
+
+function getValue(number) {
+
+  // Check to see if first value has been aquired yet
+  if (firstValueAquired == false) {
+    value1 = number;
+    resultDisplay.textContent = value1;
+    firstValueAquired = true;
+    changeButtonColor('lightgray');
+  }
+  // Once the first value has been aquired, get the second
+  else {
+    value2 = number;
+    changeButtonColor('black');
+  }
+
+}
+
+function addNumbers(a, b) {
+  first = a;
+  second = b;
+  result = a + b;
+  resultDisplay.textContent = result;
+}
+
+function equals() {
+
+  resultDisplay.textContent = result;
 }
